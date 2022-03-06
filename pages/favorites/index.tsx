5640b59df8927590e8bd740a4719b5e603d73934
GianZapata <elgianmc@hotmail.com>
2022-03-05 19:59:11 -0600

    Fin Seccion 4

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from '../../components/layouts'
+
+const FavoritesPage = () => {
+	return (
+		<MainLayout title='Pokémons - Favoritos'>
+			<h1>Favorites Page</h1>
+		</MainLayout>
+	)
+}
+
+export default FavoritesPage
